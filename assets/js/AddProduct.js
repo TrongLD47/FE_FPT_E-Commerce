b0c10c92961c80c775a9b158892962f2f5e0a0a7
Letrong181120 <91546545+TrongLD47@users.noreply.github.com>
2022-06-23 17:17:02 +0700

    add 404 notfound

---
+++ assets/js/AddProduct.js
@@ -0,0 +1,44 @@
+
+function checkSizeXs() {
+    var sizeXS = document.getElementById("sizeXs");
+    var textXS = document.getElementById("textXs");
+    if (sizeXS.checked == true) {
+        textXS.style.display = "block";
+    } else {
+        textXS.style.display = "none";
+    }
+}
+
+
+
+function checkSizeS() {
+    var sizeS = document.getElementById("sizeS");
+    var textS = document.getElementById("textS");
+    if (sizeS.checked == true) {
+        textS.style.display = "block";
+    } else {
+        textS.style.display = "none";
+    }
+}
+
+
+function checkSizeM() {
+    var sizeM = document.getElementById("sizeM");
+    var textM = document.getElementById("textM");
+    if (sizeM.checked == true) {
+        textM.style.display = "block";
+    } else {
+        textM.style.display = "none";
+    }
+}
+
+
+function checkSizeL() {
+    var sizeL = document.getElementById("sizeL");
+    var textL = document.getElementById("textL");
+    if (sizeL.checked == true) {
+        textL.style.display = "block";
+    } else {
+        textL.style.display = "none";
+    }
+}
