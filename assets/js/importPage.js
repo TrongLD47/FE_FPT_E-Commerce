1655aa778eeca41840cc2f67e30b961877c92680
Letrong181120 <91546545+TrongLD47@users.noreply.github.com>
2022-06-27 15:14:29 +0700

    change import page for saler

--- assets/js/importPage.js
+++ assets/js/importPage.js
@@ -11,3 +11,11 @@ for (var i = 0; i < includes.length; i++) {
 function load_file(filename, callback) {
   fetch(filename).then(response => response.text()).then(text => callback(text));
 }
+
+{/* <include src="./components/Header.html"></include> */}
+
+// ======================================
+$(function(){
+  $("#header").load("../html/components/Header.html"); 
+  $("#footer").load("../html/components/footer.html"); 
+});
